b7af6da7aabf1808cc656bc12477c66f40e61671
Mike Połtyn <mike@railslove.com>
2013-04-06 23:15:46 +0100

    add coordinates for most locations

--- lib/steam_location.rb
+++ lib/steam_location.rb
@@ -14,10 +14,20 @@ module SteamLocation
     country = @@countries[loccountrycode.to_s]
     if country
       map_search_array.unshift(result[:loccountry] = country['name'])
+      result[:coordinates] = country['coordinates']
+      result[:coordinates_accuracy_level] = country[:coordinates_accuracy_level]
       if state = country['states'][locstatecode.to_s]
         map_search_array.unshift(result[:locstate] = state['name'])
+        if state['coordinates']
+          result[:coordinates] = state['coordinates']
+          result[:coordinates_accuracy_level] = state[:coordinates_accuracy_level]
+        end
         if city = state['cities'][loccityid.to_s]
           map_search_array.unshift(result[:loccity] = city['name'])
+          if city['coordinates']
+            result[:coordinates] = city['coordinates']
+            result[:coordinates_accuracy_level] = city[:coordinates_accuracy_level]
+          end
         end
       end
       result[:map_search_string] = map_search_array.join(', ')
